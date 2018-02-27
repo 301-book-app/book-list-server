99b44a44db91da69c848fde57a505aadecfea1ef
Beverly Pham <beverly.pham@gmail.com>
2018-02-26 16:49:33 -0800

    no changes

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 // PORT=3000
