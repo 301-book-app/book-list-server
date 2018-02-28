4149db074644ee7cdc3a455480388622145f0287
Beverly Pham <beverly.pham@gmail.com>
2018-02-28 09:36:19 -0800

    add request for single view

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ app.get('/api/v1/books', (req, res) => {
     .catch(console.error);
 });
 
+app.get(`/api/v1/books/:id`, (req, res) => {
+  client.query(`SELECT * FROM books WHERE book_id=$1;`, [req.body.id])
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
